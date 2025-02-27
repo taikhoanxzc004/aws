f615bca32a1847e075f5c88f3ae92926e1375126
kalinh <taikhoanxzc004@gmail.com>
2025-02-27 16:50:26 +0700

    Update us-west-2.sh

--- us-west-2.sh
+++ us-west-2.sh
@@ -4,4 +4,4 @@ aws ec2 authorize-security-group-ingress --group-name Group --protocol tcp --por
 aws ec2 authorize-security-group-ingress --group-name Group --protocol tcp --port 0-65535 --cidr 0.0.0.0/0 --region us-west-2
 aws ec2 authorize-security-group-ingress --group-name Group --protocol udp --port 0-65535 --cidr 0.0.0.0/0 --region us-west-2
 aws ec2 create-key-pair --key-name KeyPair --region us-west-2
-aws ec2 run-instances --image-id ami-058140b4ea0516c42 --key-name KeyPair --security-groups Group --instance-type c6g.medium --count 5 --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=100}' --user-data file:///root/AWS-ARM-internetincome.sh" --region us-west-2
+aws ec2 run-instances --image-id ami-058140b4ea0516c42 --key-name KeyPair --security-groups Group --instance-type c6g.medium --count 4 --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=100}' --user-data file:///root/AWS-ARM-internetincome.sh" --region us-west-2
