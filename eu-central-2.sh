74eb051c6bc72882d2aa568aeacef676a01e076c
kalinh <taikhoanxzc004@gmail.com>
2025-02-27 22:40:12 +0700

    Update eu-central-2.sh

--- eu-central-2.sh
+++ eu-central-2.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-﻿#eu-central-2
 aws ec2 create-security-group --group-name Group --description "security group" --region eu-central-2
 aws ec2 authorize-security-group-ingress --group-name Group --protocol tcp --port 22 --cidr 0.0.0.0/0 --region eu-central-2
 aws ec2 authorize-security-group-ingress --group-name Group --protocol tcp --port 0-65535 --cidr 0.0.0.0/0 --region eu-central-2
 aws ec2 authorize-security-group-ingress --group-name Group --protocol udp --port 0-65535 --cidr 0.0.0.0/0 --region eu-central-2
 aws ec2 create-key-pair --key-name KeyPair --region eu-central-2
 aws ec2 run-instances --image-id ami-01ff2b0a2438155c0 --key-name KeyPair --security-groups Group --instance-type c6g.medium --count 5 --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=100}' --user-data fileb:///root/AWS-ARM-internetincome.sh --region eu-central-2
-
