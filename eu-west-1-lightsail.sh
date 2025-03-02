e3388e090cd18de11e982448944bba0f85fb0e27
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 18:03:41 +0700

    Update eu-west-1-lightsail.sh

--- eu-west-1-lightsail.sh
+++ eu-west-1-lightsail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names eu-west-1-1 --availability-zone eu-west-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-west-1
+
+aws lightsail create-instances --instance-names eu-west-1-1 --availability-zone eu-west-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-west-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name eu-west-1-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region eu-west-1
 aws lightsail open-instance-public-ports --instance-name eu-west-1-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region eu-west-1
