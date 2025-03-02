d21564e299d3f89d9f81429e5c8220b8ecfbafa9
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 18:01:20 +0700

    Update ap-south-1-lightsail.sh

--- ap-south-1-lightsail.sh
+++ ap-south-1-lightsail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-﻿aws lightsail create-instances --instance-names ap-south-1-1 --availability-zone ap-south-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_1 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-south-1
+aws lightsail create-instances --instance-names ap-south-1-1 --availability-zone ap-south-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_1 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-south-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name ap-south-1-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region ap-south-1
 aws lightsail open-instance-public-ports --instance-name ap-south-1-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region ap-south-1
