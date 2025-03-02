e391aa35d2fd571686ce9be48112c6c078ca26d4
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 18:04:22 +0700

    Update us-east-1-lightsail.sh

--- us-east-1-lightsail.sh
+++ us-east-1-lightsail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names us-east-1-1 --availability-zone us-east-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region us-east-1
+
+aws lightsail create-instances --instance-names us-east-1-2 --availability-zone us-east-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region us-east-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name us-east-1-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region us-east-1
 aws lightsail open-instance-public-ports --instance-name us-east-1-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region us-east-1
