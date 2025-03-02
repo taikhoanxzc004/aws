24377d64d068222ab31ed74ec32c88c5ad8652c3
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 14:49:50 +0700

    Update eu-north-1-lightsail.sh

--- eu-north-1-lightsail.sh
+++ eu-north-1-lightsail.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names eu-north-1-1 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
+﻿aws lightsail create-instances --instance-names eu-north-1-1 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name eu-north-1-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region eu-north-1
 aws lightsail open-instance-public-ports --instance-name eu-north-1-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region eu-north-1
 
-aws lightsail create-instances --instance-names eu-north-1-2 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
+aws lightsail create-instances --instance-names eu-north-1-2 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name eu-north-1-2 --port-info fromPort=0,protocol=TCP,toPort=65535 --region eu-north-1
 aws lightsail open-instance-public-ports --instance-name eu-north-1-2 --port-info fromPort=0,protocol=UDP,toPort=65535 --region eu-north-1
 
-aws lightsail create-instances --instance-names eu-north-1-3 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
+aws lightsail create-instances --instance-names eu-north-1-3 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name eu-north-1-3 --port-info fromPort=0,protocol=TCP,toPort=65535 --region eu-north-1
 aws lightsail open-instance-public-ports --instance-name eu-north-1-3 --port-info fromPort=0,protocol=UDP,toPort=65535 --region eu-north-1
 
-aws lightsail create-instances --instance-names eu-north-1-4 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
+aws lightsail create-instances --instance-names eu-north-1-4 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name eu-north-1-4 --port-info fromPort=0,protocol=TCP,toPort=65535 --region eu-north-1
 aws lightsail open-instance-public-ports --instance-name eu-north-1-4 --port-info fromPort=0,protocol=UDP,toPort=65535 --region eu-north-1
