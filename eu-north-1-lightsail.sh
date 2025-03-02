c6c7f331f48300c5792861fbc1b3050f3519b1bf
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 18:03:25 +0700

    Update eu-north-1-lightsail.sh

--- eu-north-1-lightsail.sh
+++ eu-north-1-lightsail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names eu-north-1-1 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
+
+aws lightsail create-instances --instance-names eu-north-1-1 --availability-zone eu-north-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region eu-north-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name eu-north-1-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region eu-north-1
 aws lightsail open-instance-public-ports --instance-name eu-north-1-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region eu-north-1
