9b3375de400a5b949cea1c19c0ff8332b73c8c92
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 18:02:22 +0700

    Update ca-central-1-lightsail.sh

--- ca-central-1-lightsail.sh
+++ ca-central-1-lightsail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names ca-central-1-1 --availability-zone ca-central-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ca-central-1
+
+aws lightsail create-instances --instance-names ca-central-1-1 --availability-zone ca-central-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ca-central-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name ca-central-1-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region ca-central-1
 aws lightsail open-instance-public-ports --instance-name ca-central-1-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region ca-central-1
