cc5d79bdc6ec9238304e93f5deb83143eee182a1
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 14:48:08 +0700

    Update ap-northeast-2-lightsail.sh

--- ap-northeast-2-lightsail.sh
+++ ap-northeast-2-lightsail.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names ap-northeast-2-1 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
+﻿aws lightsail create-instances --instance-names ap-northeast-2-1 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
 sleep 10
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region ap-northeast-2
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region ap-northeast-2
 
-aws lightsail create-instances --instance-names ap-northeast-2-2 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
+aws lightsail create-instances --instance-names ap-northeast-2-2 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
 sleep 10
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-2 --port-info fromPort=0,protocol=TCP,toPort=65535 --region ap-northeast-2
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-2 --port-info fromPort=0,protocol=UDP,toPort=65535 --region ap-northeast-2
 
-aws lightsail create-instances --instance-names ap-northeast-2-3 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
+aws lightsail create-instances --instance-names ap-northeast-2-3 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
 sleep 10
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-3 --port-info fromPort=0,protocol=TCP,toPort=65535 --region ap-northeast-2
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-3 --port-info fromPort=0,protocol=UDP,toPort=65535 --region ap-northeast-2
 
-aws lightsail create-instances --instance-names ap-northeast-2-4 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id small_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
+aws lightsail create-instances --instance-names ap-northeast-2-4 --availability-zone ap-northeast-2a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-2
 sleep 10
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-4 --port-info fromPort=0,protocol=TCP,toPort=65535 --region ap-northeast-2
 aws lightsail open-instance-public-ports --instance-name ap-northeast-2-4 --port-info fromPort=0,protocol=UDP,toPort=65535 --region ap-northeast-2
