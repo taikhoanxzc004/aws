ce4318d44724638316c5460f058537dca15cb165
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 18:05:00 +0700

    Update us-west-2-lightsail.sh

--- us-west-2-lightsail.sh
+++ us-west-2-lightsail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names us-west-2-1 --availability-zone us-west-2a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region us-west-2
+
+aws lightsail create-instances --instance-names us-west-2-1 --availability-zone us-west-2a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region us-west-2
 sleep 10
 aws lightsail open-instance-public-ports --instance-name us-west-2-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region us-west-2
 aws lightsail open-instance-public-ports --instance-name us-west-2-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region us-west-2
