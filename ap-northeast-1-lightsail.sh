f702f9ab878e5e3b2a5a5defb44449f8d582f8a7
kalinh <taikhoanxzc004@gmail.com>
2025-03-02 18:00:18 +0700

    Update ap-northeast-1-lightsail.sh

--- ap-northeast-1-lightsail.sh
+++ ap-northeast-1-lightsail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-﻿aws lightsail create-instances --instance-names ap-northeast-1-1 --availability-zone ap-northeast-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-1
+
+aws lightsail create-instances --instance-names ap-northeast-1-1 --availability-zone ap-northeast-1a --blueprint-id ubuntu_22_04 --bundle-id micro_3_0 --user-data file:///root/AWS-AMD-internetincome.sh --region ap-northeast-1
 sleep 10
 aws lightsail open-instance-public-ports --instance-name ap-northeast-1-1 --port-info fromPort=0,protocol=TCP,toPort=65535 --region ap-northeast-1
 aws lightsail open-instance-public-ports --instance-name ap-northeast-1-1 --port-info fromPort=0,protocol=UDP,toPort=65535 --region ap-northeast-1
